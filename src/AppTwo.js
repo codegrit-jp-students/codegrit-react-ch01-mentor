07c8ea4471a708bce5bf1bf9e3a33b840ec53aa0
Atsuhiro Teshima <hiro@lingualbox.com>
2019-05-11 22:35:39 +0900

    Initial commit

---
+++ src/AppTwo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AppTwo() {
+  return React.createElement('h1', null, "これは二つ目のアプリです。")
+}
+
+export default AppTwo;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import moment from 'moment-timezone';
 import './index.css';
 import App from './App';
+import AppTwo from './AppTwo';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const currentTime = moment.tz(moment(), 'Asia/Tokyo').format('YYYY年MM月DD日')
+
+ReactDOM.render(
+  React.createElement(App, {currentTime}, null), 
+  document.getElementById('root-one'));
+
+ReactDOM.render(
+  React.createElement(AppTwo, null, null), 
+  document.getElementById('root-two'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
